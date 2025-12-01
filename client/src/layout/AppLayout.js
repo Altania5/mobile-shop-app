29e704ae79eb2291481f8f2b4a634bfff77aa726
hardworkmobile <jamesferzandez@hardworkmobile.com>
2025-12-01 17:23:45 -0500

    Logo Rework3

--- client/src/layout/AppLayout.js
+++ client/src/layout/AppLayout.js
@@ -20,6 +20,7 @@ export default function AppLayout({ user, onLogout }) {
   return (
     <div className="site-wrapper">
       <nav className="dashboard-nav">
+        <img src={logo} alt="Hard Work Mobile Logo" className="nav-logo" />
         <NavLink to="/" className="nav-brand">Hard Work</NavLink>
         <button className="mobile-nav-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           &#9776;
